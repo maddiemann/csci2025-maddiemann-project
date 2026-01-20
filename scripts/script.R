7e1195092729230e2511a31ababbf0feff9ac554
maddiemann <madelyn.mann@yotes.collegeofidaho.edu>
2026-01-20 15:19:27 -0700

    check my repo

--- scripts/script.R
+++ scripts/script.R
@@ -13,7 +13,7 @@ column_names <- c(
 )
 
 # Read the data
-breast_cancer_data <- read_csv("Data/wdbc.data", col_names = column_names)
+breast_cancer_data <- read_csv("csci2025-maddiemann-project/Data/wdbc.data", col_names = column_names)
 
 glimpse(breast_cancer_data)
 
@@ -24,8 +24,7 @@ select("diagnosis", "texture_se", "area_se")
 # SE = Standard Error 
 head(breast_cancer_data)
 
-breast_cancer_data |> 
-  select("")
+
 
 # I want to do which features are the most accurate for the ML to use 
 # That way they only need to use some of the meausres not all of them 
@@ -38,4 +37,28 @@ breast_cancer_data |>
 breast_cancer_data |> 
   ggplot(aes(x = area_se, y = perimeter_mean, color = diagnosis)) +
   geom_point(alpha = 0.5)
+
+breast_cancer_data |> 
+  select("diagnosis", "symmetry_se", "concavity_se", "compactness_se") 
+  
+
+breast_cancer_data |> 
+  ggplot(aes(x = diagnosis)) + 
+  geom_bar()
+
+breast_cancer_data |> 
+  ggplot(aes(x = perimeter_mean, y = area_mean)) +
+  geom_point(alpha = 0.3, aes(color = diagnosis))+
+  geom_smooth(method = "lm")
+
+breast_cancer_data |> 
+  ggplot(aes(x = perimeter_worst, y = area_worst, color = diagnosis))+
+  geom_point(alpha = 0.3)
+
+breast_cancer_data |> 
+  ggplot(aes(x = perimeter_mean, y = area_mean)) +
+  geom_point(alpha = 0.3, aes(color = diagnosis))
+
+breast_cancer_data |> 
+  mutate()
 
